77020aee046ea66b8eddcd62f3737612dfeac2a7
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-12-07 21:23:06 +1000

    feat(day-one): completed part one

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}",fs::read_to_string("input.txt").expect("error reading input.txt").lines().map(|line| line.chars().filter(|c| c.is_numeric()).fold((None, None), |(first, last), c| match (first, last) { (None, None) => (Some(c), Some(c)), (_, _) => (first, Some(c)) })).filter_map(|(first, last)| match (first, last) { (Some(f), Some(l)) => Some([f, l]), _ => None }).map(|array| array.iter().collect::<String>().parse::<u32>().unwrap_or(0)).sum::<u32>())
 }
